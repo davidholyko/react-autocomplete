36cc8674e4241fb143c500e6256ed9c789e74a63
David Ko <david.holy.ko@gmail.com>
2020-04-13 00:31:47 -0400

    add multi key support

--- src/App.js
+++ src/App.js
@@ -5,51 +5,83 @@ import classNames from 'classnames';
 function useKeyPress(content, setCallback) {
   // State for keeping track of whether key is pressed
   const [keyPressed, setKeyPressed] = React.useState(false);
+  const [multiKeyPressed, setMultiKeyPressed] = React.useState(new Set([]));
 
   // Add event listeners
-  React.useEffect(() => {
-    // If pressed key is our target key then set to true
-    const downHandler = (event) => {
-      event.preventDefault();
-      event.stopPropagation();
-      setKeyPressed(true);
-      keyPressed && setCallback(content + event.key);
-    };
+  React.useEffect(
+    () => {
+      // If pressed key is our target key then set to true
+      const downHandler = (event) => {
+        const { key } = event;
+        console.log('!!! key down', key);
+        setKeyPressed(true);
+        setMultiKeyPressed(multiKeyPressed.add(key));
+      };
 
-    // If released key is our target key then set to false
-    const upHandler = (event) => {
-      event.preventDefault();
-      event.stopPropagation();
-      setKeyPressed(false);
-      keyPressed && setCallback(content + event.key);
-    };
+      // If released key is our target key
+      // add to content passed in and then set to false
+      const upHandler = (event) => {
+        const { key } = event;
+        console.log('!!! key up', key, keyPressed);
+        if (!keyPressed && !multiKeyPressed.has(key)) {
+          return;
+        }
+        event.preventDefault();
+        setKeyPressed(false);
+        switch (key) {
+          case 'Tab':
+            setCallback(content + '\t');
+            break;
+          case 'Enter':
+            setCallback(content + '\n');
+            break;
+          case 'Meta':
+          case 'Shift':
+            // @todo: add uppercase
+            break;
+          case 'Backspace':
+            setCallback(content.substring(0, content.length - 1));
+            break;
+          default:
+            setCallback(content + key);
+            break;
+        }
 
-    window.addEventListener('keydown', downHandler);
-    window.addEventListener('keyup', upHandler);
-    // Remove event listeners on cleanup
-    return () => {
-      window.removeEventListener('keydown', downHandler);
-      window.removeEventListener('keyup', upHandler);
-    };
-  }, [content, keyPressed, setCallback]); // Empty array ensures that effect is only run on mount and unmount
+        multiKeyPressed.delete(key);
+        setKeyPressed(multiKeyPressed);
+      };
+
+      window.addEventListener('keydown', downHandler);
+      window.addEventListener('keyup', upHandler);
+      // Remove event listeners on cleanup
+      return () => {
+        window.removeEventListener('keydown', downHandler);
+        window.removeEventListener('keyup', upHandler);
+      };
+    },
+    // Empty array ensures that effect is only run on mount and unmount
+    [content, keyPressed, setCallback],
+  );
 
   return keyPressed;
 }
 
+// eslint-disable-next-line
 const CustomEditor = (props) => {
   const [content, setContent] = React.useState('');
   const [isFocused, setFocus] = React.useState(false);
   const customEditorRef = React.useRef(null);
   const onHandleClick = (e, rest) => {
-    console.log('onHandleClick', e);
-    console.log('customEditorRef', customEditorRef);
-    customEditorRef.current.focus();
     setFocus(true);
   };
 
-  React.useEffect(() => {
-    isFocused && customEditorRef.current.focus();
-  }, [isFocused, content]);
+  React.useEffect(
+    () => {
+      isFocused && customEditorRef.current.focus();
+    },
+    //
+    [isFocused, content],
+  );
 
   useKeyPress(content, setContent);
 
@@ -60,6 +92,7 @@ const CustomEditor = (props) => {
       ref={customEditorRef}
     >
       {content}
+      <span className="cursor">|</span>
     </div>
   );
 };
